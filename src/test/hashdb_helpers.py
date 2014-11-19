ea6a060aacdbcdf5a5d0df693fc1249417e90877
Bruce Allen <bdallen@nps.edu>
2014-11-18 17:17:08 -0800

    update test

--- src/test/hashdb_helpers.py
+++ src/test/hashdb_helpers.py
@@ -2,55 +2,59 @@
 #
 # hashdb_helpers.py
 # A module for helping with hashdb tests
+from subprocess import Popen, PIPE
+
+# run command array and return lines from it
+def run_command(cmd):
+    return Popen(cmd, stdout=PIPE).communicate()[0].decode('utf-8').split("\n")
 
 # require a specific database size
-def parse_size(text):
+def parse_size(lines):
     sizes = {}
-    for line in text.splitlines():
+    for line in lines:
+        line = stream.readline()
         if line.startswith("hash store: "):
-            sizes['hash_store'] = int(line[12])
+            sizes['hash_store'] = int(line[12:])
         if line.startswith("source lookup store: "):
-            sizes['source_lookup_store'] = int(line[20])
+            sizes['source_lookup_store'] = int(line[21:])
         if line.startswith("source repository name store: "):
-            sizes['source_repository_name_store'] = int(line[29])
+            sizes['source_repository_name_store'] = int(line[30:])
         if line.startswith("source filename store: "):
-            sizes['source_filename_store'] = int(line[22])
+            sizes['source_filename_store'] = int(line[23:])
         if line.startswith("source metadata store: "):
-            sizes['source_metadata_store'] = int(line[22])
+            sizes['source_metadata_store'] = int(line[23:])
     return sizes
 
 # test import
-def parse_changes(text):
+def parse_changes(lines):
 
     # create new db
     changes = {}
-    for line in text.splitlines():
+    for line in lines:
         if line.startswith("    hashes inserted: "):
-            changes['hashes_inserted'] = int(line[20])
+            changes['hashes_inserted'] = int(line[21:])
         if line.startswith("    hashes not inserted (mismatched hash block size): "):
-            changes['hashes_not_inserted_mismatched_hash_block_size'] = int(line[53])
+            changes['hashes_not_inserted_mismatched_hash_block_size'] = int(line[54:])
         if line.startswith("    hashes not inserted (invalid byte alignment): "):
-            changes['hashes_not_inserted_invalid_byte_alignment'] = int(line[49])
+            changes['hashes_not_inserted_invalid_byte_alignment'] = int(line[50:])
         if line.startswith("    hashes not inserted (exceeds max duplicates): "):
-            changes['hashes_not_inserted_exceeds_max_duplicates'] = int(line[49])
+            changes['hashes_not_inserted_exceeds_max_duplicates'] = int(line[50:])
         if line.startswith("    hashes not inserted (duplicate element): "):
-            changes['hashes_not_inserted_duplicate_element'] = int(line[44])
+            changes['hashes_not_inserted_duplicate_element'] = int(line[45:])
         if line.startswith("    hashes removed: "):
-            changes['hashes_removed'] = int(line[19])
+            changes['hashes_removed'] = int(line[20:])
         if line.startswith("    hashes not removed (mismatched hash block size): "):
-            changes['hashes_not_removed_mismatched_hash_block_size'] = int(line[52])
+            changes['hashes_not_removed_mismatched_hash_block_size'] = int(line[53:])
         if line.startswith("    hashes not removed (invalid byte alignment): "):
-            changes['hashes_not_removed_invalid_byte_alignment'] = int(line[48])
+            changes['hashes_not_removed_invalid_byte_alignment'] = int(line[49:])
         if line.startswith("    hashes not removed (no hash): "):
-            changes['hashes_not_removed_no_hash'] = int(line[33])
+            changes['hashes_not_removed_no_hash'] = int(line[34:])
         if line.startswith("    hashes not removed (no element): "):
-            changes['hashes_not_removed_no_element'] = int(line[36])
+            changes['hashes_not_removed_no_element'] = int(line[37:])
         if line.startswith("    source metadata inserted: "):
-            changes['source_metadata_inserted'] = int(line[29])
+            changes['source_metadata_inserted'] = int(line[30:])
         if line.startswith("    source metadata not inserted (already present): "):
-            changes['source_metadata_not_inserted_already_present'] = int(line[51])
-#        print "ch1" + changes
-#    print "ch2" + changes
+            changes['source_metadata_not_inserted_already_present'] = int(line[52:])
     return changes
 
 # test for int equality
---
+++ src/test/import_export.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+#
+# Test the "New Database" command group
+
+import subprocess
+import xml.etree.ElementTree as ET
+import hashdb_helpers
+import sys
+
+db1 = "temp_1.hdb"
+
+# test import
+def test_import():
+
+    # create new db
+    subprocess.call(["rm", "-rf", db1])
+    subprocess.call(["hashdb", "create", db1])
+
+    # import block size 4096
+    lines=hashdb_helpers.run_command(["hashdb", "import", db1, "sample_dfxml4096.xml"])
+
+    changes = hashdb_helpers.parse_changes(lines)
+#    print("import_export " + str(changes["hashes_inserted"]))
+
+    hashdb_helpers.test_equals(changes["hashes_inserted"], 74)
+
+if __name__=="__main__":
+    test_import()
+
