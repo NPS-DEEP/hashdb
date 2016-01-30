4211923cf7ba6c3476988b336779d1a23961b6d4
Bruce Allen <bdallen@nps.edu>
2016-01-29 17:28:25 -0800

    add plot timestamp tool

---
+++ test_py/plot_timestamp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# graph timestamps in <hashdb.hdb>/timestamp.json
+
+import sys
+import pylab
+import matplotlib.pyplot as plot
+from argparse import ArgumentParser
+from plot_performance import read_timestamps, plot_deltas, plot_totals
+
+if __name__=="__main__":
+
+    parser = ArgumentParser(prog='plot_timestamp.py', description='Plot timestamps in timstamp.json file of hashdb')
+    parser.add_argument('hashdb_dir', help= 'hashdb directory')
+    args = parser.parse_args() 
+    hashdb_dir = args.hashdb_dir
+
+    deltas, totals = read_timestamps(hashdb_dir)
+    plot_deltas(deltas, "temp_deltas.pdf")
+    plot_totals(totals, "temp_totals.pdf")
+
